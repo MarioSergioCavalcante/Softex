61348ab343b5136189e7ca6d2bf61be6c9979970
pedroalmeida-98 <36991141+pedroalmeida-98@users.noreply.github.com>
2022-08-29 16:32:38 -0300

    New version made with A-frame and AR.Js

---
+++ dev/interface-helper.js
@@ -0,0 +1,11 @@
+function updateNumberBox(val){
+  document.getElementById("numberbox").value = val;
+}
+
+function updateSlider(val){
+  document.getElementById("tankslider").value = val;
+}
+
+function sendPos(val){
+  console.log(val);
+}
---
+++ dev/script.js
@@ -0,0 +1,18 @@
+AFRAME.registerComponent('controlvis',{
+  schema: {
+    color: {default:'#FFF'},
+  },
+  init: function () {
+    this.el.sceneEl.addEventListener('markerFound', () => {
+     
+        
+      })
+  },
+  update: function () {},
+  tick: function () {},
+  remove: function () {},
+  pause: function () {},
+  play: function () {}
+  
+});
+
